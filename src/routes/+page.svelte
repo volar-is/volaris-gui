d4112927b71b546ffcfe9682778b37325c8316a8
greendoescode <green@playlapis.com>
2024-08-16 21:46:29 +0100

    fix: changed styling for mobile devices, mobile builds wont work yet though due to plugin-dialogs not supporting android yet.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -171,44 +171,42 @@
 
 <main class="flex flex-col h-screen bg-gray-800 text-gray-100">
   <nav class="w-full bg-gray-700 shadow-md">
-    <div class="flex items-center justify-between px-8 py-4">
-      <h2 class="text-3xl font-bold text-pink-400">Volaris</h2>
-      <div class="flex items-center space-x-4">
+    <div class="flex items-center justify-between px-4 py-2 md:px-8 md:py-4">
+      <h2 class="text-2xl font-bold text-pink-400 md:text-3xl">Volaris</h2>
+      <div class="flex items-center space-x-2 md:space-x-4">
         <button
           on:click={() => (selectedTab = "file")}
-          class={`px-6 py-2 rounded-lg text-lg font-semibold flex items-center space-x-3 ${selectedTab === "file" ? "bg-purple-600 text-white" : "bg-gray-800 text-gray-300 hover:bg-gray-600"} transition-colors duration-300`}
+          class={`px-4 py-2 md:px-6 md:py-3 text-sm md:text-lg font-semibold flex items-center justify-center rounded-full ${selectedTab === "file" ? "bg-purple-600 text-white" : "bg-gray-800 text-gray-300 hover:bg-gray-600"} transition-colors duration-300`}
         >
-          <span class="material-symbols-outlined w-6 h-6">save</span>
-          <span>File</span>
+          <span class="material-symbols-outlined text-base md:text-xl">save</span>
+          <span class="hidden md:inline md:ml-2">File</span>
         </button>
         <button
           on:click={() => (selectedTab = "key")}
-          class={`px-6 py-2 rounded-lg text-lg font-semibold flex items-center space-x-3 ${selectedTab === "key" ? "bg-pink-500 text-white" : "bg-gray-800 text-gray-300 hover:bg-gray-600"} transition-colors duration-300`}
+          class={`px-4 py-2 md:px-6 md:py-3 text-sm md:text-lg font-semibold flex items-center justify-center rounded-full ${selectedTab === "key" ? "bg-pink-500 text-white" : "bg-gray-800 text-gray-300 hover:bg-gray-600"} transition-colors duration-300`}
         >
-          <span class="material-symbols-outlined w-6 h-6">key</span>
-          <span>Key File</span>
+          <span class="material-symbols-outlined text-base md:text-xl">key</span>
+          <span class="hidden md:inline md:ml-2">Key File</span>
         </button>
         <button
           on:click={toggleAction}
-          class={`px-6 py-2 rounded-lg text-lg font-semibold flex items-center space-x-3 ${actionTab === "encrypt" ? "bg-purple-600 text-white" : "bg-pink-500 text-white"} transition-colors duration-300`}
+          class={`px-4 py-2 md:px-6 md:py-3 text-sm md:text-lg font-semibold flex items-center justify-center rounded-full ${actionTab === "encrypt" ? "bg-purple-600 text-white" : "bg-pink-500 text-white"} transition-colors duration-300`}
         >
-          <span class="material-symbols-outlined w-6 h-6"
-            >{actionTab === "encrypt" ? "lock" : "no_encryption"}</span
-          >
-          <span>{actionTab === "encrypt" ? "Encrypt" : "Decrypt"}</span>
+          <span class="material-symbols-outlined text-base md:text-xl">{actionTab === "encrypt" ? "lock" : "no_encryption"}</span>
+          <span class="hidden md:inline md:ml-2">{actionTab === "encrypt" ? "Encrypt" : "Decrypt"}</span>
         </button>
         <button
           on:click={performAction}
-          class="px-6 py-2 font-semibold bg-pink-500 hover:bg-pink-400 text-white text-lg rounded-lg flex items-center space-x-3 transition-all duration-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-pink-300"
+          class="px-4 py-2 md:px-6 md:py-3 text-sm md:text-lg font-semibold bg-pink-500 hover:bg-pink-400 text-white rounded-full flex items-center justify-center transition-all duration-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-pink-300"
         >
-          <span class="material-symbols-outlined w-6 h-6">play_arrow</span>
-          <span>Run</span>
+          <span class="material-symbols-outlined text-base md:text-xl">play_arrow</span>
+          <span class="hidden md:inline md:ml-2">Run</span>
         </button>
       </div>
     </div>
   </nav>
 
-  <div class="flex-1 p-6 overflow-y-auto">
+  <div class="flex-1 p-4 md:p-6 overflow-y-auto">
     {#if selectedTab === "file"}
       <div class="flex flex-col items-center">
         <div
@@ -222,24 +220,24 @@
             isDragOver = true;
           }}
           on:dragleave={() => (isDragOver = false)}
-          class={`w-full max-w-lg border-4 ${isDragOver ? "border-purple-600 bg-gray-700" : "border-dashed border-gray-600 bg-gray-800"} rounded-lg p-12 text-center flex items-center justify-center relative transition-colors duration-300`}
-          style="min-height: 400px;"
+          class={`w-full max-w-md border-2 md:border-4 ${isDragOver ? "border-purple-600 bg-gray-700" : "border-dashed border-gray-600 bg-gray-800"} rounded-lg p-8 md:p-12 text-center flex items-center justify-center relative transition-colors duration-300`}
+          style="min-height: 300px;"
         >
-          <p class="text-gray-300 text-lg">
+          <p class="text-gray-300 text-base md:text-lg">
             Drag & drop a file here or
             <a
               on:click={pickFile}
-              class="text-purple-600 hover:text-purple-400 text-lg font-medium cursor-pointer transition-colors duration-200"
+              class="text-purple-600 hover:text-purple-400 cursor-pointer"
             >
               Select a file
             </a>
           </p>
           {#if selectedFile}
-            <p class="mt-4 text-sm text-gray-400">
+            <p class="mt-4 text-xs md:text-sm text-gray-400">
               Selected file:
               <span class="text-gray-300">{selectedFile.split("/").pop()}</span>
               <span
-                class="absolute left-1/2 transform -translate-x-1/2 top-full mt-2 bg-gray-700 text-gray-200 p-3 rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300"
+                class="absolute left-1/2 transform -translate-x-1/2 top-full mt-2 bg-gray-700 text-gray-200 p-2 md:p-3 rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300"
               >
                 {selectedFile}
               </span>
@@ -247,11 +245,11 @@
           {/if}
         </div>
 
-        <div class="mt-8 w-full max-w-lg bg-gray-700 p-6 rounded-lg shadow-lg">
-          <h3 class="text-xl font-semibold text-gray-200 mb-2">
+        <div class="mt-6 w-full max-w-md bg-gray-700 p-4 md:p-6 rounded-lg shadow-lg">
+          <h3 class="text-lg md:text-xl font-semibold text-gray-200 mb-2">
             About Volaris
           </h3>
-          <p class="text-gray-300">
+          <p class="text-gray-300 text-sm md:text-base">
             Volaris is an encryption tool designed to prioritize privacy and
             security. Built using Rust, it offers a modern and efficient
             solution for securing your data across multiple platforms, including
@@ -266,25 +264,27 @@
     {/if}
 
     {#if selectedTab === "key"}
-      <div class="flex flex-col items-center space-y-6">
-        <div class="w-full max-w-lg bg-gray-700 p-8 rounded-lg shadow-lg">
-          <h2 class="text-2xl font-semibold text-gray-100 mb-4">
+      <div class="flex flex-col items-center space-y-4 md:space-y-6">
+        <div class="w-full max-w-md bg-gray-700 p-4 md:p-6 rounded-lg shadow-lg">
+          <h2 class="text-xl md:text-2xl font-semibold text-gray-100 mb-4">
             Key File Management
           </h2>
           <div class="flex flex-col gap-4">
             <button
               on:click={saveKeyFile}
-              class="px-6 py-3 font-semibold bg-purple-600 hover:bg-purple-500 text-white text-lg rounded-lg transition-all duration-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-300"
-              style="height: 56px;"
+              class="px-4 py-2 md:px-6 md:py-3 text-base md:text-lg font-semibold bg-purple-600 hover:bg-purple-500 text-white rounded-full flex items-center justify-center transition-all duration-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-300"
+              style="height: 48px;"
             >
-              Create New Key File
+              <span class="material-symbols-outlined text-base md:text-lg">create</span>
+              <span class=" md:inline md:ml-3">Create New Key File</span>
             </button>
             <button
               on:click={pickKeyFile}
-              class="px-6 py-3 font-semibold bg-pink-500 hover:bg-pink-400 text-white text-lg rounded-lg transition-all duration-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-pink-300"
-              style="height: 56px;"
+              class="px-4 py-2 md:px-6 md:py-3 text-base md:text-lg font-semibold bg-pink-500 hover:bg-pink-400 text-white rounded-full flex items-center justify-center transition-all duration-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-pink-300"
+              style="height: 48px;"
             >
-              Select Existing Key File
+              <span class="material-symbols-outlined text-base md:text-lg">upload</span>
+              <span class=" md:inline md:ml-3">Select Existing Key File</span>
             </button>
           </div>
         </div>
